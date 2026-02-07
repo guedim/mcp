6a82ba7755dcc2b4dc43570c232f33a287fe63fb
Mario Guerrero <Mario.guerrero@192.168.80.17>
2026-02-07 12:47:21 -0500

    Initial MCP server implementation

---
+++ clase10/github-mcp-server/src/mi_server/server.py
@@ -0,0 +1,50 @@
+from starlette.applications import Starlette
+from starlette.routing import Mount, Host
+from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("My App SSE")
+
+
+@mcp.tool(
+    name="add",
+    title="add",
+    description="Return add two numbers"
+)
+def add(a: float, b:float) -> float:
+    """Add 2 numbers and return the result"""
+    return a+b
+
+@mcp.tool(
+    name="substract",
+    title="substract",
+    description="Return substract two numbers"
+)
+def substract(a: float, b:float) -> float:
+    """substract 2 numbers and return the result"""
+    return a-b
+
+@mcp.tool(
+    name="multiply",
+    title="multiply",
+    description="Return multiply two numbers"
+)
+def multiply(a: float, b:float) -> float:
+    """multiply 2 numbers and return the result"""
+    return a*b
+
+
+@mcp.tool(
+    name="divide",
+    title="divide",
+    description="Return divide two numbers"
+)
+def divide(a: float, b:float) -> float:
+    """divide 2 numers and return the result"""
+    
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    
+    return a/b
+
+
